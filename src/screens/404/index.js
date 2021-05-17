3a096dcbd064ef2d2058fb00c2a4e6f7e9fef3b2
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-05-17 01:06:47 -0300

    finalização da screen pagina não encontrada

--- src/screens/404/index.js
+++ src/screens/404/index.js
@@ -1,10 +1,33 @@
+// libs 
+import { Link } from 'react-router-dom'
+
+// icons - img
+import pagenotfound from '../../assets/pagenotfound.svg'
+
+// components 
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+// styles
+import { useStyles } from './styles'
+
 function PaginaNaoEncontrada() {
+
+    const classes = useStyles()
+
     return (
-        <>
-            <h1>404</h1>
-            <p>Pagina não encontrada</p>
-            <p>volta para a pagina principal <a href="/" >clique aqui</a></p>
-        </>
+        <Container component="main" maxWidth="sm" className={classes.root}>
+            <CssBaseline />
+            <img src={pagenotfound} className={classes.image} alt="404 Pagina não encontrada!!!" />
+            <Typography component="h1" variant="h4">
+                Pagina não encontrada
+            </Typography>
+            <br />
+            <Typography>
+                Volte para a <Link to="/" className={classes.link} >pagina principal.</Link>
+            </Typography>
+        </Container>
     )
 }
 
---
+++ src/screens/404/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        height: 600, 
+        justifyContent: 'center',  
+        alignItems: 'center',  
+        flexDirection: 'column'
+    },
+    image: {
+        width: 350, height: 350
+    },
+    link: {
+        color: '#6c63ff',
+        fontWeight: '700',
+        textDecoration: 'none'
+    }
+}))
